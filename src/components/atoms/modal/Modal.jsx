e4c21fa7c17d460024649539d2ba5e202db2a2fc
ramirosarasola <ramiro.sarasola@gmail.com>
2023-04-30 18:52:52 -0300

    handleScroll for modal

--- src/components/atoms/modal/Modal.jsx
+++ src/components/atoms/modal/Modal.jsx
@@ -8,18 +8,28 @@ const Modal = ({ children, isOpen, closeModal }) => {
 	useEffect(() => {
 		console.info("Abriendo componente Model.jsx..");
 	}, []);
+	
+	const disableScroll = () => {
+		isOpen && (document.body.style.overflow = "hidden");
+	}
+	
+	const enableScroll = () => {
+		document.body.style.overflow = "visible";
+	}
+	
+	disableScroll();
 
 	return (
 		<>
 			<article
-				className={`modal   ${isOpen && "is-open"}`}
-				onClick={closeModal}
+				className={`modal ${isOpen && "is-open"}`}
+				onClick={() => { closeModal(); enableScroll()}}
 			>
 				<div
 					className="modal-container rounded-xl b"
 					onClick={handleModalContainerClick}
 				>
-					<button className="modal-close" onClick={closeModal}>
+					<button className="modal-close" onClick={() => { closeModal(); enableScroll()}}>
 						❌
 					</button>
 					{children}
